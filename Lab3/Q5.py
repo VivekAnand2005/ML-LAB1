2e1f96f480649f21660243d0bd54512019de94ba
VivekAnand2005 <vivekanand.2714@gmail.com>
2025-07-27 21:24:54 +0530

    Create Q5.py

---
+++ Lab3/Q5.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+df = pd.read_csv('features.csv')
+df = df.select_dtypes(include=[np.number])
+
+X = df.iloc[:, :-1].values
+y_continuous = df.iloc[:, -1].values
+y = pd.qcut(y_continuous, q=3, labels=["Low", "Medium", "High"])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, y_train)
+
+print("✅ kNN model trained with k=3")
